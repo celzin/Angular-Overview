6dee846de076e316ce554e2afe017fcb1f9251ad
celso <celsovinicius4@gmail.com>
2024-05-16 15:32:35 -0300

    ...

--- my-first-project-latest/src/app/app.component.ts
+++ my-first-project-latest/src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-first-project-latest';
 }
+// <router-outlet />
