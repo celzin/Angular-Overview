0203a3438d0558c5c935d4697fff836774a9fb55
celso <celsovinicius4@gmail.com>
2024-05-16 16:17:08 -0300

    .

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponent],
   // templateUrl: './app.component.html',
   template: `
-    <router-outlet />
+    <!-- <router-outlet /> -->
     <h1>Curso de Angular</h1>
-    <h2>Componentes</h2>
+    Antigo: <app-new-component></app-new-component>
+    Novo: <app-new-component />
   `,
   // styleUrl: './app.component.scss'
 })
