519ad7ac54bbc32ef277dc99a1a7e4f8ab4a337b
celso <celsovinicius4@gmail.com>
2024-05-21 20:10:52 -0300

    seção 6

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -5,16 +5,19 @@ import { RouterOutlet } from '@angular/router';
 // Components
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
   // templateUrl: './app.component.html',
   template: `
     <!-- <router-outlet /> -->
-    <h1>Curso de Angular</h1>
-    <app-template-binding />
+    <!-- <app-new-component /> -->
+    <!-- <app-template-binding /> -->
+    <h1>Curso De Angular</h1>
+    <app-template-variables />
   `,
 })
 export class AppComponent {
--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -1,23 +1,42 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-binding',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgClass, NgStyle],
   templateUrl: './template-binding.component.html',
   styleUrl: './template-binding.component.scss'
 })
 export class TemplateBindingComponent {
   // Text Interpolation
   public name = 'Celso Vinicius';
-  public age = 22;
+  public age = 18;
   public condition = this.age > 18 ? 'Maior' : 'Menor';
-  public sum(val1:number, val2:number){
-    return val1+val2;
-  }
-
+  // public sum(val1:number, val2:number){
+  //   return val1+val2;
+  // }
+  
   // Property Binding
   public isDisabled = false;
   public srcValue = 'https://vidafullstack.com.br/wp-content/uploads/2023/12/angular-400x250.jpg';
+
+  public isTextDecoration = this.age >= 18 ? 'underline' : 'none';
+  
+  public sum() {
+    return this.age++;
+  }
+
+  public sub() {
+    return this.age--;
+  }
+
+  public oneKeyDown(event: Event){
+    return console.log(event);
+  }
+
+  public onMouseMove(event: any){
+    return console.log(event);
+  }
 }
