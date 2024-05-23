8feb711a8dd1c033a1867e50a8c7eb2e0085c365
celso <celsovinicius4@gmail.com>
2024-05-23 17:17:02 -0300

    @IF

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -6,18 +6,20 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
   // templateUrl: './app.component.html',
   template: `
     <!-- <router-outlet /> -->
     <!-- <app-new-component /> -->
     <!-- <app-template-binding /> -->
+    <!-- <app-template-variables /> -->
     <h1>Curso De Angular</h1>
-    <app-template-variables />
+    <app-template-control-flow />
   `,
 })
 export class AppComponent {
